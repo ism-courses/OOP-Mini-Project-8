743c55d22aeb9bbcef243f9b0f299007367a85ac
ism-courses <152141243+ism-courses@users.noreply.github.com>
2023-11-29 12:42:46 -0700

    Add files via upload

---
+++ OOP8_Vehicle.py
@@ -0,0 +1,8 @@
+class Vehicle:
+    
+    def __init__(self, color, speed, fuel_type):
+        self.color = color
+        self.speed = speed  # In Km/h
+        self.fuel_type = fuel_type
+
+# Write your code below:
